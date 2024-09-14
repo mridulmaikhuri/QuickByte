4aa9f680b71f7fd9988b6764acd3d65c83286386
Mridul Maikhuri <mridulmaikhuri1234@gmail.com>
2024-09-14 12:19:59 +0530

    commit

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -17,31 +17,36 @@ function Featured() {
 
   popularRecipes.sort((a: any, b: any) => b.rating - a.rating); 
   popularRecipes = popularRecipes.slice(0, 10);
+
   return (
     <div>
-      <div className='text-4xl font-serif font-bold text-red-500 ml-5 mt-3'>
+      <div className='text-5xl font-serif font-bold text-red-500 ml-6 mt-5'>
         Popular
       </div>
-      <div className='grid grid-cols-5'>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5 p-5'>
         {
           popularRecipes && popularRecipes.length > 0 ? (
             popularRecipes.map((recipe: any, index: number) => (
               <Link href={'/menu/' + recipe.id} key={index} className='text-2xl font-bold text-red-500 font-sans ml-5 mt-3'>
-                <Card className='h-[58vh]'>
+                <Card className='h-auto'>
                   <CardHeader className='h-[15vh]'>
                     <CardTitle>{recipe.name}</CardTitle>
                     <CardDescription>{recipe.cuisine}</CardDescription>
                   </CardHeader>
-                  <CardContent>
-                    <Image src={recipe.image} alt={recipe.name} width={300} height={300} />
+                  <CardContent className='flex justify-center'>
+                    <Image 
+                      src={recipe.image} 
+                      alt={recipe.name} 
+                      width={300} 
+                      height={300} 
+                      className='object-contain w-full h-auto'
+                    />
                   </CardContent>
-                  <CardFooter>
+                  <CardFooter className='flex items-center justify-start gap-2'>
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" clipRule="evenodd" d="M12 2L14.3095 8.71287L21 9.39929L16.5 14.0474L17.619 21L12 17.8529L6.38104 21L7.5 14.0474L3 9.39929L9.69048 8.71287L12 2Z" fill="gold" stroke="black" strokeWidth="1" />
                     </svg>
-                    <p>
-                      {recipe.rating}
-                    </p>
+                    <p className='text-lg'>{recipe.rating}</p>
                   </CardFooter>
                 </Card>
               </Link>
